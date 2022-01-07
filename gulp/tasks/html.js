a1505f7cd513a2a4f8af675739a0b69af59682ca
Leo <ujfox13@gmail.com>
2022-01-07 17:24:12 +0300

    nosvg webp

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,8 +1,10 @@
 import fileInclude from "gulp-file-include"; 
+import webpHtmlNosvg from "gulp-webp-html-nosvg";
 
 export const html = () => {
     return app.gulp.src(app.path.src.html) //путь к нашему главному html
         .pipe(fileInclude())    //модуль, который склеивает наши куски html    
-        .pipe(app.plugins.replace(/@img\//g, 'img/'))
+        .pipe(app.plugins.replace(/@img\//g, 'img/')) // заменям @img/ в прошакшн html файле на img/
+        .pipe(webpHtmlNosvg())
         .pipe(app.gulp.dest(app.path.build.html))//куда мы копируем файлы
 }
