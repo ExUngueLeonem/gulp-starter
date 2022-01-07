1c0b9c50437a9c0e347f6822aab1c444259abea1
Leo <ujfox13@gmail.com>
2022-01-07 21:40:31 +0300

    browser-sync

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,10 +1,12 @@
 import replace from "gulp-replace"; //поиск и замена
 import plumber from "gulp-plumber"; //обработка ошибок при работе с файлами
 import notify from "gulp-notify"; // Сообщения (подсказки)
+import browsersync from "browser-sync"; // Локальный сервер
 
 // создадим объект с плагинами и будем его экспортировать
 export const plugins = {
     replace: replace,
     plumber: plumber,
-    notify: notify
+    notify: notify,
+    browsersync: browsersync
 }
--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ global.app = {
 import { copy } from './gulp/tasks/copy.js';
 import { reset } from './gulp/tasks/reset.js';
 import { html } from './gulp/tasks/html.js';
+import { server } from './gulp/tasks/server.js';
 
 // Наблюдатель за изменениями в файловой системе watch.path
 function watcher() {
@@ -26,7 +27,7 @@ function watcher() {
 const mainTasks = gulp.parallel(copy, html);
 
 // Построение сценариев выполнения задач
-const dev = gulp.series(reset, mainTasks, watcher);
+const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
 
 // Выполенение сценария по умолчанию (здесь запускаются сценарии)
 gulp.task('default', dev);
