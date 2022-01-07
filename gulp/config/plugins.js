a56314ad04dc2a9491772401ca7fb62f98f05d6c
Leo <ujfox13@gmail.com>
2022-01-07 20:49:22 +0300

    notify and error message

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,6 +1,10 @@
 import replace from "gulp-replace"; //поиск и замена
+import plumber from "gulp-plumber"; //обработка ошибок при работе с файлами
+import notify from "gulp-notify"; // Сообщения (подсказки)
 
 // создадим объект с плагинами и будем его экспортировать
 export const plugins = {
     replace: replace,
+    plumber: plumber,
+    notify: notify
 }
--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,10 +1,33 @@
 import fileInclude from "gulp-file-include"; 
 import webpHtmlNosvg from "gulp-webp-html-nosvg";
+import versionNumber from "gulp-version-number";
 
 export const html = () => {
     return app.gulp.src(app.path.src.html) //путь к нашему главному html
+        .pipe(app.plugins.plumber(
+            app.plugins.notify.onError({
+                title: "HTML",
+                message: "Error: <%= error.message %>"
+            }))
+        )
         .pipe(fileInclude())    //модуль, который склеивает наши куски html    
         .pipe(app.plugins.replace(/@img\//g, 'img/')) // заменям @img/ в прошакшн html файле на img/
         .pipe(webpHtmlNosvg())
+        .pipe(
+            versionNumber({ // плагин версионирования добавляет таймштамп, чтобы изменения обновлялись у заказчика
+                'value': '%DT%',
+                'append': {
+                    'key': '_v',
+                    'cover': 0,
+                    'to': [
+                        'css',
+                        'js',
+                    ]
+                },
+                'output': {
+                    'file': 'gulp/version.json'
+                }
+            })
+        )
         .pipe(app.gulp.dest(app.path.build.html))//куда мы копируем файлы
 }
